99c087b45e5ffcbcaca77b727876faee01a58dc0
Tiborcode <tkelemen33@gmail.com>
2024-11-13 14:55:33 +0100

    home page set up

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-305$a@34ga!ei!5(#-&ipiml#=!_%dd1vy$8_amx^s_(inp4sp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
